41625d16af393655e78047582bdbbd3a235e5d64
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-24 21:38:38 +0600

    State Management & Toggle Functionality

--- src/conponents/AvailablePlayers/AvailablePlayers.jsx
+++ src/conponents/AvailablePlayers/AvailablePlayers.jsx
@@ -4,14 +4,14 @@ import React from "react";
 import SinglePlayer from "../SinglePlayer/SinglePlayer";
 
 const AvailablePlayers = ({ playerData }) => {
-  console.log("playerData:", playerData);
+
   return (
     <div>
-      <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+      <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
         {playerData.map((player) => {
          
           return (
-            <SinglePlayer player={player}></SinglePlayer>
+            <SinglePlayer key={player.id} player={player}></SinglePlayer>
           );
         })}
       </div>
--- src/conponents/Players/Players.jsx
+++ src/conponents/Players/Players.jsx
@@ -1,13 +1,28 @@
-import React, { use } from 'react';
-import AvailablePlayers from '../AvailablePlayers/AvailablePlayers';
+import React, { use, useState } from "react";
+import AvailablePlayers from "../AvailablePlayers/AvailablePlayers";
+import SelectedPlayers from "../SelectedPlayers/SelectedPlayers";
 
-const Players = ({playerPromise}) => {
-  const playerData = use(playerPromise)
-  console.log(playerData);
+const Players = ({ playerPromise }) => {
+  const playerData = use(playerPromise);
+
+  const [switchTab, setSwitchTab] = useState("available");
+
   return (
-    <div className='container mx-auto'>
-      player {playerData.length}
-      <AvailablePlayers playerData={playerData}></AvailablePlayers>
+    <div className="container mx-auto my-15">
+      <div className="flex justify-between items-center mb-5">
+        {switchTab === 'available' ?  <h2 className="font-bold text-2xl">Available Players</h2> : 
+        <h2 className="font-bold text-2xl">Selected Players (2/12)</h2>}
+
+        <div className="">
+          <button onClick={()=> setSwitchTab("available")} className={`btn ${switchTab === 'available' ?  'bg-[#e7fe29]' : ' '} rounded-l-xl rounded-r-none`}>
+            Available
+          </button>
+          <button onClick={()=> setSwitchTab("selected")} className={`btn ${switchTab === 'selected' ?  'bg-[#e7fe29]' : ' '} rounded-r-xl rounded-l-none`}>Selected (0)</button>
+        </div>
+      </div>
+
+
+      {switchTab === 'available' ? <AvailablePlayers playerData={playerData}></AvailablePlayers> : <SelectedPlayers></SelectedPlayers>}
     </div>
   );
 };
